37ca2b40410cbd7511dee523ae74368c7f581df9
Gabor Szabo <gabor@szabgab.com>
2014-09-16 16:48:38 +0300

    collect blog entries

--- lib/Code/Maven/Blog.pm
+++ lib/Code/Maven/Blog.pm
@@ -3,16 +3,29 @@ use Moose;
 use Path::Tiny qw(path);
 
 has dir => ( is => 'ro', required => 1 );
+has posts => ( is => 'rw' );
 
 sub collect {
 	my ($self) = @_;
 
-	my $dir = $self->dir;
-	die $dir;
+	my @posts;
+
+	my $dir   = $self->dir;
 	my @files = glob "$dir/*.txt";
 	foreach my $f (@files) {
-		path($f);
+		my @lines = path($f)->lines_utf8;
+		my %post = ( content => '' );
+		for my $line (@lines) {
+			if ( $line =~ /^=(\w+)\s+(.*?)\s*$/ ) {
+				$post{$1} = $2;
+				next;
+			}
+			$post{content} .= $line;
+		}
+		push @posts, \%post;
 	}
+	$self->posts( \@posts );
+	return scalar @posts;
 }
 
 1;
--- t/11-blog.t
+++ t/11-blog.t
@@ -2,12 +2,17 @@ use strict;
 use warnings;
 
 use Test::More;
-use Cwd qw(getcwd abs_path);
+use Cwd qw(abs_path);
 use File::Basename qw(dirname);
 
-plan tests => 1;
+plan tests => 2;
 
 use Code::Maven::Blog;
-my $blog = Code::Maven::Blog->new( dir => dirname( abs_path( getcwd() ) ) );
+my $blog = Code::Maven::Blog->new(
+	dir => dirname( dirname( abs_path($0) ) ) . '/blog' );
 isa_ok $blog, 'Code::Maven::Blog';
+
+ok $blog->collect, 'collected more than one post';
+
+#diag explain $blog->posts;
 
