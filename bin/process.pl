ebd9cddf483d8372c05656eb960a58b17ef80ccf
Gabor Szabo <gabor@szabgab.com>
2014-10-04 14:09:58 +0300

    Pod::Usage pod2usage, explain the options a bit

--- bin/process.pl
+++ bin/process.pl
@@ -4,6 +4,7 @@ use warnings;
 use Cwd qw(abs_path);
 use File::Basename qw(dirname);
 use Getopt::Long qw(GetOptions);
+use Pod::Usage qw(pod2usage);
 
 use lib 'lib';
 use Code::Maven::Config;
@@ -12,7 +13,20 @@ use Code::Maven::Source;
 my %cfg = ( root => dirname( dirname( abs_path($0) ) ), );
 GetOptions( \%cfg, 'root=s', 'source=s', 'fetch', 'zip' ) or die;
 my $sources = join '|', sort( Code::Maven::Source::sources() );
-die "--source $sources    is required\n" if not $cfg{source};
+pod2usage() if not $cfg{source};
 
 Code::Maven::Config->initialize( root => delete $cfg{root} );
 Code::Maven::Source->new(%cfg)->run;
+
+
+=head1 SYNOPSIS
+
+  --root path/to/root  (defaults to relative path)
+
+  --source $sources    is required
+
+              One or more of the actions are required:
+  --fetch
+  --zip
+
+=cut
