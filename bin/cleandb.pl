d7d5fe14078beda4e6266e5a9e2e8549b2ef2656
Gabor Szabo <gabor@szabgab.com>
2014-10-06 11:37:42 +0300

    tidy

--- bin/cleandb.pl
+++ bin/cleandb.pl
@@ -18,9 +18,11 @@ Code::Maven::Config->initialize( root => $root );
 my $db = Code::Maven::DB->new;
 if ($name) {
 	$db->clean_collection($name);
-} elsif ($all) {
+}
+elsif ($all) {
 	$db->get_db->drop;
-} else {
+}
+else {
 	die "Missing --name COLLECTION_NAME or --all\n";
 }
 
