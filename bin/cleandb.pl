66e4e68453ac9db55314533afb1c7737a589b2fa
Gabor Szabo <gabor@szabgab.com>
2014-10-06 10:50:08 +0300

    add --all flag that will drop the database

--- bin/cleandb.pl
+++ bin/cleandb.pl
@@ -11,10 +11,16 @@ use Code::Maven::DB;
 
 my $root = dirname( dirname( abs_path($0) ) );
 my $name;
-GetOptions( 'root=s' => \$root, 'name=s' => \$name ) or die;
-die "Missing --name COLLECTION_NAME\n" if not $name;
+my $all;
+GetOptions( 'root=s' => \$root, 'name=s' => \$name, 'all' => \$all ) or die;
 
 Code::Maven::Config->initialize( root => $root );
 my $db = Code::Maven::DB->new;
-$db->clean_collection($name);
+if ($name) {
+	$db->clean_collection($name);
+} elsif ($all) {
+	$db->get_db->drop;
+} else {
+	die "Missing --name COLLECTION_NAME or --all\n";
+}
 
